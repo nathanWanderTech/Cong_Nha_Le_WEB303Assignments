5b6489b4fb6989755d699532174c27d7d7146282
Nathan Le <nha.le@tekos.net>
2022-09-30 20:48:40 -0400

    Assignment 2 submit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
-// WEB303 Assignment 2
+/**
+ * WEB303 Assignment 2
+ * Student name: Cong Nha Le
+ * Student ID: 0761950
+ */
 
+async function handleLoadContent(fileLocation) {
+	const $contentEle = $('#content');
+	const xhr = new XMLHttpRequest();
+	xhr.open('GET', fileLocation, true);
+	xhr.onload = function () {
+		if (this.status === 200) {
+			$contentEle.empty();
+			$contentEle.hide();
+			$contentEle.append(xhr.responseText);
+			$contentEle.fadeIn(2000);
+		}
+	};
+	xhr.send();
+}
 
+function assignOnClickContentLinks() {
+	const $contentLinks = $('#content-wrapper a');
+	$contentLinks.each(function () {
+		$(this).on('click', function () {
+			const id = $(this).attr('id');
+			const fileLocation = `${id}.html`;
+			handleLoadContent(fileLocation);
+		});
+	});
+}
+
+$(document).ready(function () {
+	assignOnClickContentLinks();
+});
